fffcc719140f5300d299f5ff414a74fadcd65768
Longhuichen <74904186+Longhuichen@users.noreply.github.com>
2021-01-06 17:15:08 +0800

    Add files via upload

---
+++ SampleApp/SampleApp/SampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAppApp.swift
+//  SampleApp
+//
+//  Created by shark on 1/5/21.
+//
+
+import SwiftUI
+
+@main
+struct SampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
